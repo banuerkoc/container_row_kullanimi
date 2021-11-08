36fcfdf6ab2f34e0d2590a67942cfed2dbfbcb89
Ümmühan banu daşdan <ummuhanbanu.erkoc@campus.netkent.edu.tr>
2021-11-08 20:54:11 +0300

    container kullanimi

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(BenimUyg());
 }
+
+/*
 
 class  BenimUyg extends StatelessWidget {
 
@@ -11,7 +15,10 @@ class  BenimUyg extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.redAccent,
+
         body: Container(
+          padding: EdgeInsets.all(16.0),
+          margin: EdgeInsets.all(16.0),
           color: Colors.white,
           child: Text('scaffold içindeki body container text'),
         ),
@@ -20,3 +27,55 @@ class  BenimUyg extends StatelessWidget {
   }
 }
 
+*/
+
+class BenimUyg extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                color: Colors.white,
+                width: 200,
+                height: 100,
+                child: Text('birinci bölüm'),
+              ),
+              Container(
+                width: 200,
+                height: 100,
+                color: Colors.amberAccent,
+                child: Text('ikinci bölüm'),
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 200,
+                height: 100,
+                color: Colors.black,
+                child:
+                Text ('üçüncü bölüm') ,
+
+
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// ekranı sağa yaslamak için crossAxisAligment.end
+// double.infinity en geniş yeri kapla demek
+// ortaya almak için crossaxisaligment.center
+// container ın ekranı kaplaması için ( yatayda ) crossaxisaligmnet.stretch
+// sizedbox görünmez ama yer kaplar yükseklik verip margin gibi kullanabilirsin
+
+// rowda mainaxess yapıyoruz.
